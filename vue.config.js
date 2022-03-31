e0982df29e427a15290cac1b12f2823ac6cee355
happy9990929 <lala9990929@gmail.com>
2022-03-31 15:00:25 +0800

    config

--- vue.config.js
+++ vue.config.js
@@ -3,17 +3,12 @@ module.exports = {
     ? '/user-list/'
     : '/',
 
-  pages: {
-    index: {
-      // entry for the page
-      entry: 'src/main.js',
-      // the source template
-      template: 'public/index.html',
-      // output as dist/index.html
-      filename: 'index.html',
-      // when using title option,
-      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
-      title: "測驗者列表",
-    },
-  },
+    chainWebpack: config => {
+      config
+        .plugin('html')
+        .tap(args => {
+          args[0].title = "測驗者列表";
+          return args;
+        })
+    }
 };

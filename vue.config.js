5e6e08aea122143e4f2360b7f389a3087421d237
happy9990929 <lala9990929@gmail.com>
2022-03-31 14:39:50 +0800

    title

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,18 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/user-list/'
     : '/',
+
+  pages: {
+    index: {
+      // entry for the page
+      entry: 'src/main.js',
+      // the source template
+      template: 'public/index.html',
+      // output as dist/index.html
+      filename: 'index.html',
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      title: "測驗者列表",
+    },
+  },
 };
